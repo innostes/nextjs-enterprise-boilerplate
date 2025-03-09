e071437175b85f455626e4525023c83d25e634c1
innostes <bhavaraju.sasank@sganalytics.com>
2025-03-09 11:19:59 +0530

    added basic test case

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,40 +3,7 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <div className={styles['page']}>
-      basic page
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p>ajksdn</p>
+      Basic Home page
     </div>
   );
 }
